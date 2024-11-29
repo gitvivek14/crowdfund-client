a7b9e776a5276c8480a4eae640cd08f8fff2195c
Vivek <varora2004@gmail.com>
2024-11-29 08:47:16 +0530

    msg

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './fonts/Futura/Futura-Bold.ttf';
 import {ClerkProvider} from "@clerk/clerk-react"
 
 // const chainID = 11155111;
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = "pk_test_cGxlYXNpbmctY29yZ2ktOTAuY2xlcmsuYWNjb3VudHMuZGV2JA"
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
